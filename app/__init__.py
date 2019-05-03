c9328cf3c3f041391ed6a5d98fbebc2d2dd51d63
Neil Marshall <Neil.Marshall@riskfirst.com>
2019-05-03 09:12:24 +0100

    move to application factory

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,20 @@
 from flask import Flask
-from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-from app import routes, models
+from app.routes import app_bp
+from app import models
+from config import Config
+
+def create_app(config_object=Config):
+
+    app = Flask(__name__)
+    app.config.from_object(config_object)
+
+    db.init_app(app)
+
+    app.register_blueprint(app_bp)
+
+    return app
+
--- run.py
+++ run.py
@@ -1,1 +1,9 @@
-from app import app
+from app import create_app, db
+from app.models import Filepaths
+
+app = create_app()
+
+@app.shell_context_processor
+
+def make_shell_context():
+    return {'db': db, 'Filepaths': Filepaths}
