a784e250fec9d484cc1d83e8b7ad069561eca6fe
neilmarshall <neil.marshall@dunelm.org.uk>
2019-05-06 18:12:53 +0100

    Add 'User' model

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+login = LoginManager()
 migrate = Migrate()
 
 from app.routes import app_bp
@@ -14,6 +16,7 @@ def create_app(config_object=Config):
     app.config.from_object(config_object)
 
     db.init_app(app)
+    login.init_app(app)
     migrate.init_app(app, db)
 
     app.register_blueprint(app_bp)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
-from app import db
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from app import db, login
 
 class Problem(db.Model):
 
@@ -23,3 +26,26 @@ class Language(db.Model):
 
     def __repr__(self):
         return f"Languages(language_id={self.language_id}, language={self.language})"
+
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(255), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    def __repr__(self):
+        return f"User(id={self.id}, username={self.username})"
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
