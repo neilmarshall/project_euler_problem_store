8ae0f48732906776f68da86f15a226800a7dfd5a
neilmarshall <neil.marshall@dunelm.org.uk>
2019-05-06 18:12:53 +0100

    Add 'Language' model

--- app/models.py
+++ app/models.py
@@ -6,6 +6,20 @@ class Problem(db.Model):
 
     problem_id = db.Column(db.Integer, primary_key=True)
     contents = db.Column(db.Text, nullable=False)
+    language = db.Column(db.Integer, db.ForeignKey('languages.language_id'), nullable=False)
 
     def __repr__(self):
-        return f"Filepaths(id={self.id}, contents={self.contents})"
+        return f"Problem(problem_id={self.id}, contents={self.contents})"
+
+
+class Language(db.Model):
+
+    __tablename__ = "languages"
+
+    language_id = db.Column(db.Integer, primary_key=True)
+    language = db.Column(db.String(16), nullable=False)
+
+    problems = db.relationship('Filepaths', lazy=True, backref='language')
+
+    def __repr__(self):
+        return f"Languages(language_id={self.language_id}, language={self.language})"
