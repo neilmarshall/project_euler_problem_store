82e85cd135b6c3e673df8aa6da2305875c8ba31f
neilmarshall <neil.marshall@dunelm.org.uk>
2019-05-06 18:29:03 +0100

    update flask 'make_shell_context' function

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ class Problem(db.Model):
     language = db.Column(db.Integer, db.ForeignKey('languages.language_id'), nullable=False)
 
     def __repr__(self):
-        return f"Problem(problem_id={self.id}, contents={self.contents})"
+        return f"Problem(problem_id={self.problem_id}, contents={self.contents}, language={self.language})"
 
 
 class Language(db.Model):
@@ -21,8 +21,6 @@ class Language(db.Model):
 
     language_id = db.Column(db.Integer, primary_key=True)
     language = db.Column(db.String(16), nullable=False)
-
-    problems = db.relationship('Filepaths', lazy=True, backref='language')
 
     def __repr__(self):
         return f"Languages(language_id={self.language_id}, language={self.language})"
--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from app import create_app, db
-from app.models import Problem
+from app.models import Language, Problem, User
 
 app = create_app()
 
 @app.shell_context_processor
 
 def make_shell_context():
-    return {'db': db, 'Filepaths': Filepaths}
+    return {'db': db, 'Problem': Problem, 'Language': Language, 'User': User}
