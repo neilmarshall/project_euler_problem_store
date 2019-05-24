32e26b2f3ebe72bb94f208a3343efdc6bf0bed5e
neilmarshall <neil.marshall@dunelm.org.uk>
2019-05-24 22:16:18 +0100

    add tests covering page acess restrictions

---
+++ tests/test_access_to_restricted_pages.py
@@ -0,0 +1,38 @@
+import unittest
+
+from app import create_app, db
+from app.models import Language, Problem, User
+
+class TestConfig():
+    SECRET_KEY = "123456"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SOLUTIONS_TO_SHOW = 12
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+
+
+class TestLandingPage(unittest.TestCase):
+
+    def setUp(self):
+        # establish application context and test client
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.test_client = self.app.test_client()
+
+    def tearDown(self):
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_accessing_create_solution_page_without_logging_in_returns_status_code_401(self):
+        response = self.test_client.get('/create_solution', follow_redirects=True)
+        self.assertEqual(response.status_code, 401)
+
+    def test_accessing_update_solution_page_without_logging_in_returns_status_code_401(self):
+        response = self.test_client.get('/update_solution', follow_redirects=True)
+        self.assertEqual(response.status_code, 401)
+
+    def test_accessing_delete_solution_page_without_logging_in_returns_status_code_401(self):
+        response = self.test_client.get('/delete_solution', follow_redirects=True)
+        self.assertEqual(response.status_code, 401)
