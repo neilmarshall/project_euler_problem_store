0f348242a3ab25eb0738d61f9d22238169d2b5a7
neilmarshall <neil.marshall@dunelm.org.uk>
2019-05-30 23:10:48 +0100

    update 'search_query' route functionality

--- app/search_route.py
+++ app/search_route.py
@@ -1,18 +1,26 @@
-from flask import Blueprint, redirect, request, url_for
+from flask import Blueprint, redirect, render_template, request, url_for
 from app import db
-from app.models import Language, Problem, User
-from app.routes import app_bp
+from app.models import Problem
 
 search_bp = Blueprint('search_bp', __name__)
 
 @search_bp.route('/search')
 def search_router():
-    search_string = request.args.get('search_for', None)
-    if search_string:
-        search_result = db.session \
-                          .query(Problem.problem_id) \
-                          .filter(Problem.contents.ilike(f'%{search_string}%')) \
-                          .all()
-        search_result = map(lambda result: result[0], search_result)
-        print(list(search_result))
-    return redirect(url_for('app_bp.index'))
+    search_for = request.args.get('search_for', None)
+
+    if not search_for:
+        return redirect(url_for('app_bp.index'))
+
+    search_results = db.session \
+                       .query(Problem.problem_id) \
+                       .filter(Problem.contents.ilike(f'%{search_for}%')) \
+                       .all()
+
+    search_results = [result[0] for result in search_results]
+
+    if not search_results:
+        return render_template('search_results_null.html')
+
+    return render_template('search_results.html',
+                           search_results=search_results,
+                           search_for=search_for)
