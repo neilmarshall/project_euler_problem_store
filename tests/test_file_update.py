924c5cb23766117cbb732284474cee641ba082f2
neilmarshall <neil.marshall@dunelm.org.uk>
2019-06-01 19:49:20 +0100

    add tests checking file updating

---
+++ tests/test_file_update.py
@@ -0,0 +1,81 @@
+import unittest
+from io import BytesIO
+
+from app import create_app, db
+from app.models import Language, Problem, User
+
+class TestConfig():
+    SECRET_KEY = "123456"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SOLUTIONS_TO_SHOW = 12
+    TESTING = True
+    WTF_CSRF_ENABLED = False
+
+
+class TestFileUpdating(unittest.TestCase):
+
+    def setUp(self):
+        # establish application context and test client
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.test_client = self.app.test_client()
+
+        # create in-memory database structure
+        db.create_all()
+
+        # add a User object
+        self.user1 = User(username='user1')
+        self.user1.set_password('pass1')
+        db.session.add(self.user1)
+
+        # add a Language object
+        self.language = Language(language_id=1, language='Python', extension='py')
+        db.session.add(self.language)
+
+        db.session.commit()
+
+        # log in
+        self.test_client.post('/', data = {'username': 'user1', 'password': 'pass1'})
+
+        # add a problem
+        self.data = {'problem_selection': 1, 'problem_title': 'a title',
+                'file_upload': (BytesIO(b'file contents'), 'test.py')}
+        self.test_client.post('/create_solution', data=self.data)
+
+    def tearDown(self):
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_updating_file_without_specifiying_title_overrides_contents_only(self):
+        data = {'problem_selection': 1, 'file_update': (BytesIO(b'new contents'), 'test.py')}
+        response = self.test_client.post('/update_solution', follow_redirects=True, data=data)
+        self.assertEqual(response.status_code, 200)
+        db_problem = Problem.query.first()
+        self.assertEqual(Problem.query.count(), 1)
+        self.assertEqual(db_problem.title, self.data['problem_title'])
+        self.assertEqual(db_problem.contents, 'new contents')
+        self.assertEqual(db_problem.language_id, self.language.language_id)
+
+    def test_updating_file_overrides_contents_and_title(self):
+        data = {'problem_selection': 1, 'problem_title': 'new title',
+                'file_update': (BytesIO(b'new contents'), 'test.py')}
+        response = self.test_client.post('/update_solution', follow_redirects=True, data=data)
+        self.assertEqual(response.status_code, 200)
+        db_problem = Problem.query.first()
+        self.assertEqual(Problem.query.count(), 1)
+        self.assertEqual(db_problem.title, 'new title')
+        self.assertEqual(db_problem.contents, 'new contents')
+        self.assertEqual(db_problem.language_id, self.language.language_id)
+
+    def test_updating_non_existent_file_does_nothing(self):
+        data = {'problem_selection': 2, 'problem_title': 'new title',
+                'file_update': (BytesIO(b'new contents'), 'test.py')}
+        response = self.test_client.post('/update_solution', follow_redirects=True, data=data)
+        self.assertEqual(response.status_code, 200)
+        db_problem = Problem.query.first()
+        self.assertEqual(Problem.query.count(), 1)
+        self.assertEqual(db_problem.title, self.data['problem_title'])
+        self.assertEqual(db_problem.contents, 'file contents')
+        self.assertEqual(db_problem.language_id, self.language.language_id)
