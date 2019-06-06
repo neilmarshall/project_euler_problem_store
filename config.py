6d4f2adf336ba306ba406a202178f236a5c38371
Neil Marshall <Neil.Marshall@riskfirst.com>
2019-06-06 13:07:58 +0100

    pylint 'config.py'

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
+"""
+Production configuration for Flask Application
+"""
 import os
+
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
-class Config(object):
+class Config():
+    """ Production configuration for Flask Application """
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
