0540921f8c0d55e12e2da0ffd634017b0b9703f2
Neil Marshall <Neil.Marshall@riskfirst.com>
2019-06-06 13:07:58 +0100

    pylint 'run.py'

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
+"""
+Entry point for 'flask run' command
+"""
 from app import create_app, db
 from app.models import Language, Problem, User
 
 app = create_app()
 
 @app.shell_context_processor
-
 def make_shell_context():
+    """Launch a Python interpreter pre-populated with an application context"""
     return {'db': db, 'Problem': Problem, 'Language': Language, 'User': User}
