42ac658242e6b52cc72e1f314215273a7e44d796
neilmarshall <neil.marshall@dunelm.org.uk>
2019-08-09 20:00:22 +0100

    update config.py to pre-ping MySQL database

--- config.py
+++ config.py
@@ -13,4 +13,5 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {'pool_pre_ping': True}
     SOLUTIONS_TO_SHOW = 12
