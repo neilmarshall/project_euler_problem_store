c54219425033de28d15412d9fbd70af7d39b81a1
neilmarshall <neil.marshall@dunelm.org.uk>
2019-09-10 22:04:56 +0100

    clear form inputs on successfully creating problem

--- app/views/create_route.py
+++ app/views/create_route.py
@@ -18,32 +18,48 @@ def create_solution():
     for extension in db.session.query(Language.extension).all():
         file_upload_form.allowed_extensions.append(extension[0])
 
-    if file_upload_form.validate_on_submit():
+    if file_upload_form.is_submitted():
+        if file_upload_form.validate():
 
-        problem_id = file_upload_form.data.get('problem_selection')
+            problem_id = file_upload_form.data.get('problem_selection')
 
-        # check problem_id doesn't already exist
-        if Problem.query.filter_by(problem_id=problem_id).first() is not None:
-            flash("A solution for that problem already exists - please specify an unsolved "
-                  "problem or update the solution", "warning")
-
-        else:
-            title = file_upload_form.data.get('problem_title')
-            contents = file_upload_form.file_upload.data.read()
-            contents = contents.decode('utf-8').replace('\r\n', '\n')
-
-            # check content is not null
-            if not contents:
-                flash("File must not be empty", "warning")
+            # check problem_id doesn't already exist
+            if Problem.query.filter_by(problem_id=problem_id).first() is not None:
+                flash("A solution for that problem already exists - please specify an unsolved "
+                      "problem or update the solution", "warning")
 
             else:
-                extension = file_upload_form.file_upload.data.filename.split('.')[-1]
-                language = Language.query.filter_by(extension=extension).first()
-                language_id = language.language_id
-                problem = Problem(problem_id=problem_id, contents=contents,
-                                  language_id=language_id, title=title)
-                db.session.add(problem)
-                db.session.commit()
-                flash("Solution created", "success")
+                title = file_upload_form.data.get('problem_title')
+                contents = file_upload_form.file_upload.data.read()
+                contents = contents.decode('utf-8').replace('\r\n', '\n')
+
+                # check content is not null
+                if not contents:
+                    flash("File must not be empty", "warning")
+
+                else:
+                    # create objects to add to database
+                    extension = file_upload_form.file_upload.data.filename.split('.')[-1]
+                    language = Language.query.filter_by(extension=extension).first()
+                    language_id = language.language_id
+                    problem = Problem(problem_id=problem_id, contents=contents,
+                                      language_id=language_id, title=title)
+
+                    # add objects to database
+                    db.session.add(problem)
+                    db.session.commit()
+
+                    # clear form values
+                    file_upload_form.problem_selection.raw_data = ['']
+                    file_upload_form.problem_title.data = None
+
+                    flash("Solution created", "success")
+
+        else:
+            error_messages = [f.process_errors[0] for f in file_upload_form._fields.values() if f.process_errors]
+            if error_messages:
+                flash(error_messages[0], "warning")
+            else:
+                flash("An error was encountered - please review inputs and try again", "warning")
 
     return render_template('create_solution.html', file_upload_form=file_upload_form)
